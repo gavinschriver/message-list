1dc179468866aee29a19165676a18a9244eef8bf
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-26 14:05:11 -0500

    initial setup

---
+++ scripts/messages/MessageList.js
@@ -0,0 +1,34 @@
+import { useMessages } from "./MessageProvider.js"
+import { Message } from "./Message.js"
+
+const contentTarget = document.querySelector(".messages")
+
+/*
+    COMPONENT FUNCTION
+*/
+export const MessageList = () => {
+    const allMessages = useMessages()
+    render(allMessages)
+}
+
+/*
+    RENDERING FUNCTION
+*/
+const render = messageArray => {
+    const convertedMessages = messageArray.map(messageObject => {
+        const messageHTML = Message(messageObject)
+        return messageHTML
+    })
+    const combinedSections = convertedMessages.join("")
+    contentTarget.innerHTML = combinedSections
+}
+
+document.querySelector(".themes").addEventListener("click", e => {
+    const idOfClickedElement = e.target.id
+
+    if (idOfClickedElement.startsWith("themeButton--")) {
+        const [prefix, color] = idOfClickedElement.split("--")
+        contentTarget.classList = []
+        contentTarget.classList.add(color)
+    }
+})
