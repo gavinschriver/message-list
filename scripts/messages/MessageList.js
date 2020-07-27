3df90dd670c845ec67f4f5f834ef5534bd101b9e
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-26 21:16:20 -0500

    broke it by changing themebutton to btn--theme

--- scripts/messages/MessageList.js
+++ scripts/messages/MessageList.js
@@ -20,7 +20,7 @@ const render = messageArray => {
         return messageHTML
     })
     const combinedSections = convertedMessages.join("")
-    contentTarget.innerHTML = combinedSections
+    contentTarget.innerHTML = combinedSections 
 }
 
 document.querySelector(".themes").addEventListener("click", e => {
@@ -32,3 +32,6 @@ document.querySelector(".themes").addEventListener("click", e => {
         contentTarget.classList.add(color)
     }
 })
+
+//the event handler for the theme buttons is tied to a contentTarget area assigned to .messages; we have 'color', coming from a theme button, being added to the classList of the div where "mesages" go
+//this means that, if the themeButton--whateverColor class gets renamed, the startsWith condition in that block above won't be true, and thus won't assign a color
